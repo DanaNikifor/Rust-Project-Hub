762a2fcd4b2708259254f972aa73c3f79e807ad6
Dana Nikiforov <nionovnadasa@gmail.com>
2024-01-28 20:16:21 +0300

    yes

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def start(massage):
 
 @bot.message_handler()
 def massage_reader(massage):
-	if(massage.text == "Привет" or "привет" or "прив" or "Прив"):
+	if(massage.text in ("Привет", "привет", "прив", "Прив")):
 		bot.send_message(massage.chat.id, f"Привет, {massage.from_user.first_name}")
+	else:
+		bot.send_message(massage.chat.id, f"{massage.from_user.first_name}, напиши привет")
+		print(massage.text)
 
 bot.polling(none_stop=True) 
