a67d1990b172e99d5a712ab69759da78366b8e36
Dana Nikiforov <nionovnadasa@gmail.com>
2024-02-05 19:21:44 +0300

    Expirement

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import telebot
+from telebot import types
 import Token
 
 bot = telebot.TeleBot(Token.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(massage):
-	bot.send_message(massage.chat.id, '<b>Добро Пожаловать!</b>', parse_mode='html')
+	mp = types.ReplyKeyboardMarkup()
+	mp.add((types.KeyboardButton("Поздороваться")))
+	welcome = f"Добро Пожаловать, {massage.from_user.first_name}!"
+	bot.send_message(massage.chat.id, welcome, parse_mode='html', reply_markup=mp)
 
 @bot.message_handler()
 def massage_reader(massage):
-	if(massage.text in ("Привет", "привет", "прив", "Прив")):
+	if(massage.text in ("Привет", "привет", "прив", "Прив", "Поздороваться")):
 		bot.send_message(massage.chat.id, f"Привет, {massage.from_user.first_name}")
 	else:
-		bot.send_message(massage.chat.id, f"{massage.from_user.first_name}, напиши привет")
-		print(massage.text)
+		bot.send_message(massage.chat.id, f"😠{massage.from_user.first_name}, используй кнопочки")
+	print(massage.text)
 
 while True:
 	bot.polling() 
