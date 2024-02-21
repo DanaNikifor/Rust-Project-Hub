60851024f06cff848445742250142bf18d399f54
DanaNikifor <155372421+DanaNikifor@users.noreply.github.com>
2024-02-21 18:28:05 +0300

    version0.0.2

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from telebot import types
 import Token
 
 bot = telebot.TeleBot(Token.TOKEN)
+keys = ("Поздороваться", "Правила")
 
 @bot.message_handler(commands=['start'])
 def start(massage):
-	mp = types.ReplyKeyboardMarkup()
-	mp.add((types.KeyboardButton("Поздороваться")))
+	mp = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	hello = types.KeyboardButton("Поздороваться")
+	guide = types.KeyboardButton("Правила")
+	mp.add(hello, guide)
 	welcome = f"Добро Пожаловать, {massage.from_user.first_name}!"
 	bot.send_message(massage.chat.id, welcome, parse_mode='html', reply_markup=mp)
 
 @bot.message_handler()
 def massage_reader(massage):
-	if(massage.text in ("Привет", "привет", "прив", "Прив", "Поздороваться")):
-		bot.send_message(massage.chat.id, f"Привет, {massage.from_user.first_name}")
+	if(massage.text in keys):
+		if massage.text == "Поздороваться":
+			bot.send_message(massage.chat.id, f"Привет, {massage.from_user.first_name}")
+		if massage.text == "Правила":
+			bot.send_message(massage.chat.id, f"Используйте кнопочки чтобы управлять ботом")
 	else:
 		bot.send_message(massage.chat.id, f"😠{massage.from_user.first_name}, используй кнопочки")
 	print(massage.text)
